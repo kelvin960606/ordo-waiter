aeb66d7939574d13bd3226d03f5bdada79532d76
TienPing <petwesley@gmail.com>
2019-04-23 22:28:45 +0800

    add test for pricetag

---
+++ app/components/PriceTag/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import PriceTag from '../index';
+
+describe('<PriceTag />', () => {
+    it('Expect to have unit tests specified', () => {
+        expect(true).toEqual(false);
+    });
+});
