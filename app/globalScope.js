622645d004a68a8f0744fad721ee0be22f4160cd
TienPing <petwesley@gmail.com>
2019-04-25 19:13:29 +0800

    add potrait handling for list

--- app/globalScope.js
+++ app/globalScope.js
@@ -1,10 +1,13 @@
+import { Dimensions } from 'react-native';
+
 // default value for globalScope
 export const globalScope = {
     api: 'http://ordo-koa.herokuapp.com',
     isConnected: true,
     productData: null,
     token: '',
-    numColumnForSmallCard: 4,
+    numColumnForSmallCard: Dimensions.get('window').width > 1050 ? 4 : 3,
     marginForSmallCard: 1,
     paddingForSmallCard: 2,
+    tempToken: 'b91edc20-0026-4b8f-b2ab-02b3622dc5cd',
 };
